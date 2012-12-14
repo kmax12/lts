117e25bd19fb08ecc737c6c9fde8ff172df4b518
kanter <kanter@kanter.(none)>
2012-12-13 22:35:05 -0500

    profile

--- lts/settings.py
+++ lts/settings.py
@@ -1,5 +1,6 @@
 # Django settings for lts project.
-DEBUG = True
+import os
+DEBUG = False
 try:
     from local_settings import *
 except ImportError:
@@ -59,7 +60,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- lts/urls.py
+++ lts/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from reg_form import UserRegistrationForm
 from registration.views import register
 import regbackend
+
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -9,6 +11,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^register/$', register, {'backend': 'registration.backends.simple.SimpleBackend','form_class': UserRegistrationForm}, name='registration_register'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 	(r'', include('registration.backends.simple.urls')),
 	url(r'', include('lifetime.urls')),
     # Examples:
