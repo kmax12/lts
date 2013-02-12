861c07bc6bffba413d68aee88b9ed75d64680efc
Colin Sidoti <csidoti@Colins-MacBook-Pro.local>
2013-02-11 23:25:34 -0500

    500 and 404

--- lifetime/templatetags/ship_tags.py
+++ lifetime/templatetags/ship_tags.py
@@ -4,5 +4,8 @@ register = Library()
 
 @register.filter
 def can_ship(user, product): 
-	return user.profile.has_product_id(product.id)
+	if user.is_authenticated():
+		return user.profile.has_product_id(product.id)
+
+	return None
     
