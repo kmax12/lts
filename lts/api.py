0011f1688133d4ee4fcfc840e50cbb16061fd4c8
Max Kanter <max@lifetimesupply.com>
2013-04-09 16:26:21 -0400

    supplies

--- lts/api.py
+++ lts/api.py
@@ -3,9 +3,31 @@ from tastypie import fields
 from tastypie.models import ApiKey
 from tastypie.resources import ModelResource
 from lifetime.models import Supply, Category, Product, ProductImage
-from tastypie.authentication import BasicAuthentication
-from tastypie.authorization import DjangoAuthorization
+from tastypie.authentication import BasicAuthentication, ApiKeyAuthentication
+from tastypie.authorization import DjangoAuthorization, Authorization, ReadOnlyAuthorization
+from django.contrib.auth import authenticate, login, logout
+from tastypie.http import HttpUnauthorized, HttpForbidden
+from django.conf.urls import url
+from tastypie.utils import trailing_slash
 
+class SupplyAuthorization(ReadOnlyAuthorization):
+    def is_authorized(self, request, object=None):
+       return false
+
+    # Optional but useful for advanced limiting, such as per user.
+    def apply_limits(self, request, object_list):
+        return []
+
+class SupplyAuthentication( ApiKeyAuthentication ):
+    '''
+    Authenticates everyone if the request is GET otherwise performs
+    ApiKeyAuthentication.
+    '''
+
+    def is_authenticated(self, request, **kwargs):
+        if request.GET.get("all", "true") == 'true':
+            return True
+        return super( SupplyAuthentication, self ).is_authenticated( request, **kwargs )
 
 
 class ProductResource(ModelResource):
@@ -27,9 +49,30 @@ class SupplyResource(ModelResource):
             full=True
     )
 
+
+    def get_object_list(self, request):
+        if request.GET.get("all", "true") == "true":
+            return Supply.objects.all()
+        elif request.user:
+            return request.user.profile.get_supplies()
+
+
+    def apply_authorization_limits(self, request, object_list):
+        print "works!"
+        if request.user.is_superuser:
+            return object_list.filter(user__id=request.GET.get('user__id',''))
+
+    # def apply_authorization_limits(self, request, object_list):
+    #     # if request.GET.get("all", "true") != "true":
+    #     #     return User.objects.filter(user=request.user).profile.get_supplies()
+    #     return []
+
     class Meta:
         queryset = Supply.objects.select_related().all()
+        authorization = ReadOnlyAuthorization()
+        authentication = SupplyAuthentication()
         resource_name = 'supply'
+
 
 class ProductImageResource(ModelResource):
     product = fields.ForeignKey(ProductResource, 'product')
@@ -38,12 +81,6 @@ class ProductImageResource(ModelResource):
         queryset = ProductImage.objects.all()
         resource_name = 'product_image'
 
-
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
-from tastypie.http import HttpUnauthorized, HttpForbidden
-from django.conf.urls import url
-from tastypie.utils import trailing_slash
 
 class UserResource(ModelResource):
     class Meta:
@@ -71,3 +108,4 @@ class UserResource(ModelResource):
 #     class Meta:
 #         queryset = Entry.objects.all()
 #         resource_name = 'entry'
+
